d3068ce94738fb4f351bf1bb71c304ab2d93be09
Joel Mathew <mathew.145@buckeyemail.osu.edu>
2022-03-05 12:47:07 -0500

    Navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/Home";
+import Products from "./Products/Products";
 function App() {
 
   const openMenu = () => {
@@ -12,103 +14,45 @@ function App() {
   }
 
   return (
-    <div className="grid-container">
-        <head>
-            <link rel="stylesheet" href="style.css" />
-            <title>Violent Fairy</title>
-        </head>
-        <body>
-            <div>
-            <header className = "header">
-                <div className = "brand">
-                    <button onClick={openMenu}>&#9776;</button>
-                    <a href="index.html">Violent Fairy</a>
-                </div>
-                <div className = "header-links">
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">Sign In</a>
-                </div>
-            </header>
-            <aside className = "sidebar">
-                <h3>Shopping Categories</h3>
-                <button className = "sidebar-close-button" onClick={closeMenu}>x</button>
-                <ul>
-                    <li>
-                        <a href="index.html">Pants</a>
-                    </li>
+    <Router>   
+        <div className="grid-container">
+                <link rel="stylesheet" href="style.css" />
+                <title>Violent Fairy</title>
 
-                    <li>
-                        <a href="index.html">Shirts</a>
-                    </li>
-                </ul>
-            </aside>
-            <main>
-                <div className = "content">
-                    <ul className = "products">
+            <body>
+                <header className="header">
+                    <div className="brand">
+                        <button onClick={openMenu}>&#9776;</button>
+                        <a href="index.htm">Violent Fairy</a>
+                    </div>
+                    <div className="header-links">
+                        <Link to="/">Home</Link>
+                        <Link to="/catalog">Catalog</Link>
+                    </div>
+                </header>
+                <aside className="sidebar">
+                    <h3>Shopping Categories</h3>
+                    <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                    <ul>
                         <li>
-                            <div className = "product">
-                                <img className = "product-image" src = "images/d1.jpg" alt="product" />
-                                <div className = "product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className = "product-brand">Nike</div>
-                                <div className = "product-price">$60</div>
-                                <div className = "product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
+                            <a href="index.html">Pants</a>
                         </li>
+
                         <li>
-                            <div className = "product">
-                                <img className = "product-image" src = "images/d1.jpg" alt="product" />
-                                <div className = "product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className = "product-brand">Nike</div>
-                                <div className = "product-price">$60</div>
-                                <div className = "product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className = "product">
-                                <img className = "product-image" src = "images/d1.jpg" alt="product" />
-                                <div className = "product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className = "product-brand">Nike</div>
-                                <div className = "product-price">$60</div>
-                                <div className = "product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className = "product">
-                                <img className = "product-image" src = "images/d1.jpg" alt="product" />
-                                <div className = "product-name">
-                                <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className = "product-brand">Nike</div>
-                                <div className = "product-price">$60</div>
-                                <div className = "product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className = "product">
-                                <img className = "product-image" src = "images/d1.jpg" alt="product" />
-                                <div className = "product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className = "product-brand">Nike</div>
-                                <div className = "product-price">$60</div>
-                                <div className = "product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
+                            <a href="index.html">Shirts</a>
                         </li>
                     </ul>
-                </div>
-            </main>
-            <footer>
-                &copy; 2022 Violent Fairy
-            </footer>
+                </aside>
+                <main className="main">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/catalog" element={<Products />} />
+                    </Routes>
+                </main>
+                <footer className="footer">&copy; 2022 Violent Fairy</footer>
+            </body>
         </div>
-    </body>
-    </div>
+    </Router>
   );
 }
 
