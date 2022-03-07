05cb4c3e99aa0420ec2d5cad76e3602fbf319414
Nicholas Pirro <60107736+Bunta2@users.noreply.github.com>
2022-03-07 16:02:30 -0500

    Add files via upload
    with payment added

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/Home";
+import Products from "./Products/Products";
+import data from './Products/data';
+import Orders from './Orders/Orders';
+import Payment from "./Payment/Payment";
+import OrderDetail from './Orders/OrderDetail';
+function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+  }
+
+  return (
+    <Router>
+        <div className="grid-container">
+            <link rel="stylesheet" href="style.css" />
+            <title>Violent Fairy</title>
+            <body>
+            <header className="header">
+            <div className="brand">
+                <button onClick={openMenu}>&#9776;</button>
+                <a href="index.htm">Violent Fairy</a>
+            </div>
+            <div className="header-links">
+                <Link to ="/">Home</Link>
+                <Link to ="/catalog">Catalog</Link>
+                <Link to ="/orders">Orders</Link>
+                <Link to = "/payment">Payment</Link>
+            </div>
+            </header>
+            <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+                <li>
+                    <a href="index.html">Pants</a>
+                </li>
+
+                <li>
+                    <a href="index.html">Shirts</a>
+                </li>
+            </ul>
+            </aside>
+            <main className="main">
+                <Routes>
+                    <Route path = "/" element={<Home />} />
+                    <Route path = "/catalog" element={<Products />} />
+                    <Route path = "orders" element={<Orders />} ></Route>
+                    <Route path = "/order/:id" element={<OrderDetail />} ></Route>
+                    <Route path = "payment" element={<Payment/>} ></Route>
+                </Routes>
+            </main>
+            <footer className="footer">&copy; 2022 Violent Fairy</footer>
+        </body>
+        </div>
+    </Router>
+  );
+}
+
+export default App;
